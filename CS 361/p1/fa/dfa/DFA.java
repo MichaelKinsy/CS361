8194289378f4eb5036f36b3818933db4d66fe2c6
Michael Kinsy <54590707+MichaelKinsy@users.noreply.github.com>
2021-10-11 23:48:23 -0600

    finished implementing

--- CS 361/p1/fa/dfa/DFA.java
+++ CS 361/p1/fa/dfa/DFA.java
@@ -1,22 +1,23 @@
 package fa.dfa;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import fa.State;
 public class DFA implements DFAInterface {
     private DFAState q0;
-    private HashSet<DFAState> q;
-    private HashSet<DFAState> f;
-    private HashMap<DFAState, HashMap<Character, DFAState>> sigma;
+    private LinkedHashSet<DFAState> q;
+    private LinkedHashSet<DFAState> f;
+    private HashMap<DFAState, HashMap<Character, DFAState>> delta;
+    private LinkedHashSet<Character> sigma;
 
     public DFA(){
-        this.q = new HashSet<DFAState>();
+        this.q = new LinkedHashSet<DFAState>();
         this.q0 = null;
-        this.f =  new HashSet<DFAState>();
-        this.sigma =  new HashMap<DFAState,HashMap<Character,DFAState>>();
+        this.f =  new LinkedHashSet<DFAState>();
+        this.sigma = new LinkedHashSet<Character>();
+        this.delta =  new HashMap<DFAState,HashMap<Character,DFAState>>();
     }
 
     @Override
@@ -37,20 +38,28 @@ public class DFA implements DFAInterface {
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
         HashMap<Character,DFAState> transition = new HashMap<Character,DFAState>();
+        this.sigma.add(onSymb);
         transition.put(onSymb, new DFAState(toState));
-        this.sigma.put(new DFAState(fromState),transition);
+        if(this.delta.containsKey(new DFAState(fromState))){
+            this.delta.get(new DFAState(fromState)).put(onSymb, new DFAState(toState));
+        }else{
+            this.delta.put(new DFAState(fromState),transition);
+        }
+        
     }
 
     @Override
     public Set<DFAState> getStates() {
-        HashSet<DFAState> states = new HashSet<DFAState>();
+        LinkedHashSet<DFAState> states = new LinkedHashSet<DFAState>();
+        
+        for (DFAState state : this.f){
+            states.add(state);
+        }
         states.add(this.q0);
         for (DFAState state : this.q){
             states.add(state);
         }
-        for (DFAState state : this.f){
-            states.add(state);
-        }
+        
         return states;
     }
 
@@ -65,23 +74,83 @@ public class DFA implements DFAInterface {
     }
 
     @Override
-    public Set<Character> getABC() {
+    public LinkedHashSet<Character> getABC() {
         return sigma;
     }
 
     @Override
     public boolean accepts(String s) {
-        boolean accept = true;
+        boolean accept = false;
+        DFAState state = this.q0;
         for (char sym: s.toCharArray()){
-            
+            state = getToState(state, sym);
+            if (state == null){
+                return accept;
+            }
         }
+        if(this.f.contains(state)){
+            accept = true;
+        }
+
         return accept;
     }
 
     @Override
-    public State getToState(fa.dfa.DFAState from, char onSymb) {
-        // TODO Auto-generated method stub
-        return null;
+    public DFAState getToState(fa.dfa.DFAState from, char onSymb) {
+        if( this.delta.get(from) == null || this.delta.get(from).get(onSymb) == null){
+            return null;
+        }
+        return this.delta.get(from).get(onSymb);
+    }
+
+    @Override
+    public String toString(){
+        String result = "Q = { ";
+        Iterator itr = this.f.iterator();
+        while(itr.hasNext()){
+            result += itr.next() + " ";
+        }
+        if (!this.f.contains(this.q0)){
+            result += this.q0 + " ";
+        }
+
+        itr = this.q.iterator();
+        while(itr.hasNext()){
+            result += itr.next() + " ";
+        }
+
+        result += "}\nSigma = { ";
+
+        itr = this.sigma.iterator();
+        while(itr.hasNext()){
+            result+= itr.next() + " ";
+        }
+        result += "}\ndelta = \n\t\t";
+        itr = this.sigma.iterator();
+        while(itr.hasNext()){
+            result += itr.next() + "\t";
+        }
+        result += "\n\t";
+        itr =getStates().iterator();
+        while(itr.hasNext()){
+            Iterator itr2 = this.sigma.iterator();
+            DFAState state = (DFAState) itr.next();
+            result += state + "\t";
+            while(itr2.hasNext()){
+                Character symb = (Character) itr2.next();
+                result += getToState(state,symb) + "\t";
+            }
+            result += "\n\t";
+            
+        }
+        result += "\nq0 = " + this.q0 + "\nF = { ";
+        itr = this.f.iterator();
+        while(itr.hasNext()){
+            result += itr.next() + " ";
+        }
+        
+        result += "}\n";
+        return result;
     }
  
 }
--- CS 361/p1/fa/dfa/DFAState.java
+++ CS 361/p1/fa/dfa/DFAState.java
@@ -3,8 +3,20 @@ package fa.dfa;
 import fa.State;
 
 public class DFAState extends State {
-    private String name;
     public DFAState(String name){
         this.name = name;
+    }
+
+    @Override 
+    public boolean equals(Object obj){
+        if(obj instanceof DFAState){
+            return this.name.equals(((DFAState)obj).name);
+        }return false;
+    }
+
+    @Override
+    public int hashCode(){
+        int prime = 31;
+        return prime + ((name == null) ? 0 : name.hashCode());
     }
 }
